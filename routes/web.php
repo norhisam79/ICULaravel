75fc0d0c27b4ffad521ad75359c27eae3f505b4b
norhisam79 <norhisam79@gmail.com>
2024-10-15 10:38:16 +0800

    1. Update APP Name
    2.  Update title with param value
    
    3. Update Named Route with value param

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,53 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/home/{name}', function ($name) {
+    return view('home', ['name' => $name]);
+});
+
 Route::get('auth/signin', function () {
     return view('auth.signin');
+});
+
+//Named Route
+Route::get('/user/profile', function () {
+    return 'Pengguna Profile';
+})->name('user.profile');
+
+//Route Param
+Route::get('/user/{name}', function ($name) {
+    return 'User '.$name;
+});
+
+// alias of a route user.profile = /pengguna/profile
+
+//Redirect route to named route
+Route::get('/redirect-to-profile', function () {
+    return redirect()->route('user.profile');
+});
+
+//Route Group
+Route::prefix('food')->group(function () {
+
+    Route::get('/details', function () {
+        return 'Food details are following';
+    });
+
+    Route::get('/home', function () {
+        return 'Food home page';
+    });
+    
+});
+
+//combination of all above (Route, Named Route, Route Param, Route Group, Route Prefix)
+Route::name('job')->prefix('job')->group(function () {
+
+    Route::get('home', function () {
+        return 'Job home page';
+    })->name('.home');
+
+    Route::get('details', function () {
+        return 'Job details re following';
+    })->name('.description');
+    
 });
