3e6924dd5b5daee2457aa0640df6a26eabefb1fe
norhisam79 <norhisam79@gmail.com>
2024-10-15 15:16:43 +0800

    update 2nd day pagi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{config('app.name')}}</title>
-</head>
-<body>
-    <h1>Hello, {{$name ?? "team ICU"}}</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('content')
+@php($_name = $name ?? "team")
+
+    @if($_name == "samz")
+    <p>You are banned</p>
+    @else
+    <h1>Hello, {{ $_name ?? "team" }}</h1>
+    @endif
+@endsection
+
+
+
